f4c2626a70df39a4db7eaabd7bc4a799ef7c86ad
Flightsimmer50 <mohamedmaamoun.jrad@esprit.tn>
2024-08-28 20:56:42 +0100

    Consuming Add Restaurant

--- screens/AddRestaurant.js
+++ screens/AddRestaurant.js
@@ -1,55 +1,35 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Alert, StyleSheet, ScrollView, TouchableOpacity, Image, SafeAreaView } from 'react-native';
-import * as ImagePicker from 'expo-image-picker'; // Utilisation de expo-image-picker
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Alert, StyleSheet, ScrollView, TouchableOpacity, SafeAreaView } from 'react-native';
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 
 const AddRestaurantPage = () => {
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
+    userName: '',
     phone: '',
     email: '',
     nameRes: '',
     address: '',
     cuisineType: '',
     taxeTPS: '',
-    taxeTVQ: '',
+    taxeTUQ: '', // Changer de taxeTVQ à taxeTUQ
     payCashMethod: '',
-    images: '', // Ajout de l'état pour l'URL de l'image
+    tokenLogin: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0Y2MzMGJhMmY0ODdmZDA0MzhjZjM2ZiIsInJvbGUiOiJyZXNGcmFuY2hpc2UiLCJpYXQiOjE3MjQ4NjY1NDksImV4cCI6MTcyNTQ3MTM0OX0.RiMMaCbEOWqBlz8lNp5gAotzjjtXoj7zzQHwiwlvKaQ', // Valeur par défaut
   });
 
-  const navigation = useNavigation(); // Utilisation de useNavigation pour accéder aux fonctions de navigation
+  const navigation = useNavigation();
 
-  // Fonction pour gérer les changements dans les champs de texte
   const handleChange = (e, fieldName) => {
     setFormData({ ...formData, [fieldName]: e.nativeEvent.text });
   };
 
-  // Fonction pour ouvrir la bibliothèque d'images et sélectionner une image
-  const pickImage = async () => {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== 'granted') {
-      console.log('Sorry, we need camera roll permissions to make this work!');
-      return;
-    }
-    
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      // Met à jour l'état avec l'URI de l'image sélectionnée
-      setFormData({ ...formData, images: result.uri });
-    }
-  };
-
-  // Fonction pour valider les champs du formulaire avant la soumission
   const validateForm = () => {
-    const { firstName, lastName, phone, email, nameRes, address, cuisineType, taxeTPS, taxeTVQ, payCashMethod } = formData;
+    const { firstName, lastName, userName, phone, email, nameRes, address, cuisineType, taxeTPS, taxeTUQ, payCashMethod } = formData;
 
-    if (!firstName || !lastName || !phone || !email || !nameRes || !address || !cuisineType || !taxeTPS || !taxeTVQ || !payCashMethod) {
+    if (!firstName || !lastName || !userName || !phone || !email || !nameRes || !address || !cuisineType || !taxeTPS || !taxeTUQ || !payCashMethod) {
       Alert.alert("Validation Error", "All fields are required. Please fill out all fields.");
       return false;
     }
@@ -57,40 +37,25 @@ const AddRestaurantPage = () => {
     return true;
   };
 
-  // Fonction pour gérer la soumission du formulaire
   const handleSubmit = async () => {
     if (!validateForm()) {
-      return; // Stop the submission if validation fails
+      return;
     }
 
-    // Création d'une instance de FormData
-    const formDataWithImage = new FormData();
-    for (const key in formData) {
-      formDataWithImage.append(key, formData[key]);
-    }
-    
-    // Ajout de l'image au FormData
-    if (formData.images) {
-      formDataWithImage.append('images', {
-        uri: formData.images,
-        type: 'image/jpeg', // Ajustez le type MIME en fonction de l'image sélectionnée
-        name: 'photo.jpg',
-      });
-    }
+    console.log('FormData being sent:', formData); // Log the formData to the console
 
     try {
-      // Envoi de la requête POST pour ajouter le restaurant
-      const response = await axios.post('http://192.168.1.17:5555/user/addRestaurant', formDataWithImage, {
+      const response = await axios.post('http://192.168.1.17:5555/user/addRestaurantFranchise', formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          'Content-Type': 'application/json',
         },
         withCredentials: true,
       });
 
       Alert.alert("Success", response.data.message || "Restaurant added successfully");
-      navigation.goBack(); // Retourne à la page précédente après un ajout réussi
+      navigation.goBack();
     } catch (error) {
-      console.error("Error adding restaurant:", error);
+      console.error("Error adding restaurant:", error.response ? error.response.data : error.message);
       Alert.alert("Error", "Failed to add restaurant. Please try again.");
     }
   };
@@ -105,13 +70,6 @@ const AddRestaurantPage = () => {
           <Text style={styles.title}>Add a New Restaurant</Text>
         </View>
         <View style={styles.formContainer}>
-          <TouchableOpacity onPress={pickImage} style={styles.imagePickerButton}>
-            {formData.images ? (
-              <Image source={{ uri: formData.images }} style={styles.imagePreview} />
-            ) : (
-              <Text style={styles.imagePickerText}>Select an Image</Text>
-            )}
-          </TouchableOpacity>
           <TextInput
             style={styles.input}
             placeholder="First Name"
@@ -123,6 +81,12 @@ const AddRestaurantPage = () => {
             placeholder="Last Name"
             value={formData.lastName}
             onChange={(e) => handleChange(e, 'lastName')}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="UserName"
+            value={formData.userName}
+            onChange={(e) => handleChange(e, 'userName')}
           />
           <TextInput
             style={styles.input}
@@ -162,9 +126,9 @@ const AddRestaurantPage = () => {
           />
           <TextInput
             style={styles.input}
-            placeholder="TVQ Tax"
-            value={formData.taxeTVQ}
-            onChange={(e) => handleChange(e, 'taxeTVQ')}
+            placeholder="TUQ Tax" // Modifier le placeholder de taxeTVQ à taxeTUQ
+            value={formData.taxeTUQ}
+            onChange={(e) => handleChange(e, 'taxeTUQ')}
           />
           <TextInput
             style={styles.input}
@@ -184,7 +148,7 @@ const AddRestaurantPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FAF3F3', // Couleur de fond saumon pâle
+    backgroundColor: '#FAF3F3',
   },
   scrollContainer: {
     flexGrow: 1,
@@ -197,11 +161,11 @@ const styles = StyleSheet.create({
     marginBottom: 24,
   },
   backButton: {
-    backgroundColor: '#FA8072', // Bouton retour saumon
+    backgroundColor: '#FA8072',
     borderRadius: 4,
     padding: 10,
     alignItems: 'center',
-    marginRight: 16, // Espacement entre le bouton et le titre
+    marginRight: 16,
   },
   backButtonText: {
     fontSize: 18,
@@ -211,7 +175,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     flex: 1,
     textAlign: 'center',
-    color: '#FA8072', // Couleur saumon
+    color: '#FA8072',
     fontWeight: 'bold',
   },
   formContainer: {
@@ -222,29 +186,11 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 4, // Pour Android
-  },
-  imagePickerButton: {
-    backgroundColor: '#FAF3F3',
-    borderColor: '#FA8072',
-    borderWidth: 1,
-    borderRadius: 8,
-    padding: 10,
-    alignItems: 'center',
-    marginBottom: 16,
-  },
-  imagePreview: {
-    width: 100,
-    height: 100,
-    borderRadius: 8,
-  },
-  imagePickerText: {
-    color: '#FA8072',
-    fontSize: 16,
+    elevation: 4,
   },
   input: {
     height: 45,
-    borderColor: '#FA8072', // Bordure saumon
+    borderColor: '#FA8072',
     borderWidth: 1,
     borderRadius: 4,
     marginBottom: 16,
@@ -253,7 +199,7 @@ const styles = StyleSheet.create({
     color: '#333',
   },
   submitButton: {
-    backgroundColor: '#FA8072', // Bouton ajouter saumon
+    backgroundColor: '#FA8072',
     borderRadius: 4,
     paddingVertical: 12,
     alignItems: 'center',
